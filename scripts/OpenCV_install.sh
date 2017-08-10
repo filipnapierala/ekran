cac130dfbc9185f1291a1d7826b42ddf0dfa7ebb
rtyma <tymarobert25@gmail.com>
2017-08-10 18:52:54 +0200

    add scripts folder

--- scripts/OpenCV_install.sh
+++ scripts/OpenCV_install.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
 sudo apt-get install build essential
-sudo apt-get install libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
