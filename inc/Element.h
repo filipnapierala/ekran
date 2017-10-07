8d04ba8c870194d7122f6210e207dbdaedf68a9d
rtyma <tymarobert25@gmail.com>
2017-10-07 17:36:17 +0200

    add further class (TODO: screen counter)

---
+++ inc/Element.h
@@ -0,0 +1,17 @@
+/*
+ * Element.h
+ *
+ *  Created on: Oct 7, 2017
+ *      Author: robert
+ */
+
+#ifndef ELEMENT_H_
+#define ELEMENT_H_
+
+class Element {
+public:
+	Element();
+	virtual ~Element();
+};
+
+#endif /* ELEMENT_H_ */
---
+++ src/Element.cpp
@@ -0,0 +1,18 @@
+/*
+ * Element.cpp
+ *
+ *  Created on: Oct 7, 2017
+ *      Author: robert
+ */
+
+#include "Element.h"
+
+Element::Element() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Element::~Element() {
+	// TODO Auto-generated destructor stub
+}
+
