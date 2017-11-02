de80f1fb47e412dc6f360e204d1ff16505d73c04
filipnapierala <filnap@tlen.pl>
2017-11-02 12:18:13 +0100

    my first commit, new dimensioning

--- main.cpp
+++ main.cpp
@@ -64,19 +64,19 @@ void screen1()
 	gui->add_screen();
 
 	gui->screen_vector[gui->actual_screen]->add_button("/crio",50,25,"crio");
-	gui->screen_vector[gui->actual_screen]->add_button("/hot",250,150,"hot");
-	gui->screen_vector[gui->actual_screen]->add_button("/manual",350,350,"manual");
+	gui->screen_vector[gui->actual_screen]->add_button("/hot",150,250,"hot");
+	gui->screen_vector[gui->actual_screen]->add_button("/manual",340,350,"manual");
 	gui->screen_vector[gui->actual_screen]->add_button("/else",0,460,"else");
-	gui->screen_vector[gui->actual_screen]->add_button("/diag",1180,50,"diag");
+	gui->screen_vector[gui->actual_screen]->add_button("/diag",1215,35,"diag");
 	gui->screen_vector[gui->actual_screen]->add_button("/reset",1180,700,"reset");
 
-	gui->screen_vector[gui->actual_screen]->add_image("/alarm",1230,50,"alarm");
+	gui->screen_vector[gui->actual_screen]->add_image("/alarm",1170,37,"alarm");
 
-#define x 25
-	gui->screen_vector[gui->actual_screen]->add_image("/fan",650+x,480,"fan");
-	gui->screen_vector[gui->actual_screen]->add_image("/red",790+x,500,"red");
-	gui->screen_vector[gui->actual_screen]->add_image("/blue",890+x,500,"blue");
-	gui->screen_vector[gui->actual_screen]->add_image("/pr",1030+x,480,"pr");
+#define x 920
+	gui->screen_vector[gui->actual_screen]->add_image("/fan",x-220,480,"fan");
+	gui->screen_vector[gui->actual_screen]->add_image("/red",x-75,500,"red");
+	gui->screen_vector[gui->actual_screen]->add_image("/blue",x+25,500,"blue");
+	gui->screen_vector[gui->actual_screen]->add_image("/pr",x+145,480,"pr");
 
 	gui->screen_vector[gui->actual_screen]->add_video("/vid",600,100,"video",640,360);
 }
@@ -84,24 +84,26 @@ void screen1()
 void screen2()
 {
 	gui->add_screen();
-
+#undef x
+#define x 640
+#define y 400
 	gui->screen_vector[gui->actual_screen]->add_button("/ret",1130,0,"ret");
 	gui->screen_vector[gui->actual_screen]->add_button("/start",0,650,"start");
 
-	gui->screen_vector[gui->actual_screen]->add_button("/minus",200,330,"minus1");
-	gui->screen_vector[gui->actual_screen]->add_button("/minus",200,400,"minus2");
-	gui->screen_vector[gui->actual_screen]->add_button("/minus",200,470,"minus3");
-	gui->screen_vector[gui->actual_screen]->add_button("/plus",1100,330,"plus1");
-	gui->screen_vector[gui->actual_screen]->add_button("/plus",1100,400,"plus2");
-	gui->screen_vector[gui->actual_screen]->add_button("/plus",1100,470,"plus3");
+	gui->screen_vector[gui->actual_screen]->add_button("/minus",x-490,y-75,"minus1");
+	gui->screen_vector[gui->actual_screen]->add_button("/minus",x-490,y-5,"minus2");
+	gui->screen_vector[gui->actual_screen]->add_button("/minus",x-490,y+65,"minus3");
+	gui->screen_vector[gui->actual_screen]->add_button("/plus",x+460,y-85,"plus1");
+	gui->screen_vector[gui->actual_screen]->add_button("/plus",x+460,y-15,"plus2");
+	gui->screen_vector[gui->actual_screen]->add_button("/plus",x+460,y+55,"plus3");
 
-	gui->screen_vector[gui->actual_screen]->add_trackbar("/r1","/s1",240,330,255,"trackbar1");
-	gui->screen_vector[gui->actual_screen]->add_trackbar("/r1","/s1",240,400,255,"trackbar2");
-	gui->screen_vector[gui->actual_screen]->add_trackbar("/r1","/s1",240,470,255,"trackbar3");
+	gui->screen_vector[gui->actual_screen]->add_trackbar("/r1","/s1",240,y-80,255,"trackbar1");
+	gui->screen_vector[gui->actual_screen]->add_trackbar("/r1","/s1",240,y-10,255,"trackbar2");
+	gui->screen_vector[gui->actual_screen]->add_trackbar("/r1","/s1",240,y+60,255,"trackbar3");
 
-	gui->screen_vector[gui->actual_screen]->add_image("/hot",100,330,"hot");
-	gui->screen_vector[gui->actual_screen]->add_image("/cold",100,400,"cold");
-	gui->screen_vector[gui->actual_screen]->add_image("/time",100,470,"time");
+	gui->screen_vector[gui->actual_screen]->add_image("/hot",40,y-85,"hot");
+	gui->screen_vector[gui->actual_screen]->add_image("/cold",40,y-15,"cold");
+	gui->screen_vector[gui->actual_screen]->add_image("/time",40,y+55,"time");
 }
 
 void screen3()
@@ -109,33 +111,41 @@ void screen3()
 	gui->add_screen();
 
 	gui->screen_vector[gui->actual_screen]->add_button("/ret",1130,0,"ret");
-	gui->screen_vector[gui->actual_screen]->add_button("/on",390,250,"on1");
-	gui->screen_vector[gui->actual_screen]->add_button("/on",790,250,"on2");
-	gui->screen_vector[gui->actual_screen]->add_button("/on",390,550,"on3");
-	gui->screen_vector[gui->actual_screen]->add_button("/off",490,250,"off1");
-	gui->screen_vector[gui->actual_screen]->add_button("/off",890,250,"off2");
-	gui->screen_vector[gui->actual_screen]->add_button("/off",490,550,"off3");
-	gui->screen_vector[gui->actual_screen]->add_button("/push",700,500,"push");
-
-	gui->screen_vector[gui->actual_screen]->add_image("/blue",340,150,"blue");
-	gui->screen_vector[gui->actual_screen]->add_image("/red",840,150,"red");
-	gui->screen_vector[gui->actual_screen]->add_image("/fan",340,450,"fan");
-	gui->screen_vector[gui->actual_screen]->add_image("/crio",700,400,"crio");
+#undef y
+#define y 213
+	gui->screen_vector[gui->actual_screen]->add_button("/on",x-282,y,"on1");
+	gui->screen_vector[gui->actual_screen]->add_button("/off",x-187,y,"off1");
+	gui->screen_vector[gui->actual_screen]->add_button("/on",x+113,y,"on2");
+	gui->screen_vector[gui->actual_screen]->add_button("/off",x+213,y,"off2");
+#undef y
+#define y 613	
+	gui->screen_vector[gui->actual_screen]->add_button("/on",x-282,y,"on3");	
+	gui->screen_vector[gui->actual_screen]->add_button("/off",x-187,y,"off3");
+	gui->screen_vector[gui->actual_screen]->add_button("/push",x+150,y-13,"push");
+#undef y
+#define y 400
+	gui->screen_vector[gui->actual_screen]->add_image("/blue",x-225,y-262,"blue");
+	gui->screen_vector[gui->actual_screen]->add_image("/red",x+175,y-262,"red");
+	gui->screen_vector[gui->actual_screen]->add_image("/fan",x-225,y+138,"fan");
+	gui->screen_vector[gui->actual_screen]->add_image("/crio",x+175,y+138,"crio");
 }
 
 void screen4()
 {
 	gui->add_screen();
-
+#undef y
+#define y 220
 	gui->screen_vector[gui->actual_screen]->add_button("/ret",1130,0,"ret");
-	gui->screen_vector[gui->actual_screen]->add_button("/pr1",140,220,"pr1");
-	gui->screen_vector[gui->actual_screen]->add_button("/pr2",440,220,"pr2");
-	gui->screen_vector[gui->actual_screen]->add_button("/pr3",740,220,"pr3");
-	gui->screen_vector[gui->actual_screen]->add_button("/pr4",1040,220,"pr4");
-	gui->screen_vector[gui->actual_screen]->add_button("/pr5",140,580,"pr5");
-	gui->screen_vector[gui->actual_screen]->add_button("/pr6",440,580,"pr6");
-	gui->screen_vector[gui->actual_screen]->add_button("/pr7",740,580,"pr7");
-	gui->screen_vector[gui->actual_screen]->add_button("/pr8",1040,580,"pr8");
+	gui->screen_vector[gui->actual_screen]->add_button("/pr1",x-500,y,"pr1");
+	gui->screen_vector[gui->actual_screen]->add_button("/pr2",x-200,y,"pr2");
+	gui->screen_vector[gui->actual_screen]->add_button("/pr3",x+100,y,"pr3");
+	gui->screen_vector[gui->actual_screen]->add_button("/pr4",x+400,y,"pr4");
+#undef y
+#define y 480
+	gui->screen_vector[gui->actual_screen]->add_button("/pr5",x-500,y,"pr5");
+	gui->screen_vector[gui->actual_screen]->add_button("/pr6",x-200,y,"pr6");
+	gui->screen_vector[gui->actual_screen]->add_button("/pr7",x+100,y,"pr7");
+	gui->screen_vector[gui->actual_screen]->add_button("/pr8",x+400,y,"pr8");
 }
 
 int main()
